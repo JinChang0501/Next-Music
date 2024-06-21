5fa0ef84d2f1ae3f5b1ef2cab50bf950a0fe87d0
Efron <a161026521@gmail.com>
2024-06-21 11:44:36 +0800

    DefaultLayout add container & delete nav file & change nav.js name & globals.scss add container variables $ index.js test container success

--- next-music/pages/index.js
+++ next-music/pages/index.js
@@ -2,20 +2,15 @@ import { products } from '@/data/products'
 export default function Index() {
   return (
     <>
+    <div style={{height:'100px'}} className="w-100 bg-purple1 text-warning mt-3 d-flex justify-content-center align-items-center ">測試 container : 桌機版 1600px ; 手機版 334px</div>
       <table className="table table-bordered table-striped table-hover mt-3">
         <thead>
           <tr>
             <th>#</th>
-            <th>author</th>
             <th>bookname</th>
-            <th>category_sid</th>
-            <th>book_id</th>
             <th>publish_date</th>
             <th>pages</th>
             <th>price</th>
-            <th>isbn</th>
-            <th>on_sale</th>
-            <th>introduction</th>
           </tr>
         </thead>
         <tbody>
@@ -23,16 +18,10 @@ export default function Index() {
             return (
               <tr key={v.id}>
                 <td>{v.id}</td>
-                <td>{v.author}</td>
                 <td>{v.bookname}</td>
-                <td>{v.category_sid}</td>
-                <td>{v.book_id}</td>
                 <td>{v.publish_date}</td>
                 <td>{v.pages}</td>
                 <td>{v.price}</td>
-                <td>{v.isbn}</td>
-                <td>{v.on_sale}</td>
-                <td>{v.introduction}</td>
               </tr>
             )
           })}
