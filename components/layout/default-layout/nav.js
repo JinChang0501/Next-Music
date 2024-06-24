55cf90abd1bd24b8b8554722bf5daabb69c80800
Castle9707 <paomincc25@gmail.com>
2024-06-24 23:14:17 +0800

    0624 navbar

--- components/layout/default-layout/nav.js
+++ components/layout/default-layout/nav.js
@@ -1,8 +1,119 @@
 import React from 'react'
+import { BsPersonCircle, BsBell, BsCart } from "react-icons/bs";
 
 export default function Nav() {
   return (
     <>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
+        aria-label="Eighth navbar example"
+        style={{ borderBottom: "1px solid var(--Primary-03, #DBD7FF)" }}
+      >
+        <div className="container">
+          {/* Logo */}
+          <a className="navbar-brand" href="/">
+            Logoo
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          {/* 分頁連結 */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="activity.html"
+                >
+                  演出活動
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="product.html">
+                  周邊商城
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="artist.html">
+                  音樂人
+                </a>
+              </li>
+            </ul>
+            {/* 右方icon */}
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 icon-lg-size">
+              <li className="icon-margin">
+                <a href="#" className="nav-link">
+                  <BsCart />
+                </a>
+              </li>
+              <li className="icon-margin dropdown">
+                <a
+                  className="nav-link"
+                  href="#"
+                  id="navbarDropdown01"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <BsBell />
+                </a>
+                <ul
+                  className="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="navbarDropdown01"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      通知1
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      通知2
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="icon-margin dropdown">
+                <a
+                  className="nav-link"
+                  href="#"
+                  id="navbarDropdown02"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <BsPersonCircle />
+                </a>
+                <ul
+                  className="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="navbarDropdown02"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      會員中心
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      登出
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <nav className="navbar navbar-expand-lg bg-purple3">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
