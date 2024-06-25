654484d06bf8a0e768ad5085ed5c456588c06737
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-06-26 01:12:21 +0800

    新增-收藏庫卡片元件 / module.scss

---
+++ components/member/computer-layout/collection-card.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './collection-card.module.scss'
+
+export default function CollectionCard() {
+  return (
+    <>
+      <div className={styles['collection-card']}>
+        <div className="card" style={{ width: '18rem' }}>
+          <button className="close-btn">
+            <i className="bi bi-x-circle-fill"></i>
+          </button>
+          <img
+            src={`/images/member/img/composing-2391033__340.jpg`}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <div
+              className={`${styles['text-truncate-ellipsis']} ${styles['card-title']}`}
+            >
+              一生到底，One Life, One Shot
+            </div>
+            <p
+              className={`${styles['multiline-ellipsis']} ${styles['card-text']}`}
+            >
+              中文字測試中文字測試中文字測試中文字測試中文字測試中文字測試中文字測試中文字測試中文字測試
+            </p>
+            <div className="d-flex">
+              <a href="#" className="text-decoration-none ms-auto">
+                查看更多...
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
