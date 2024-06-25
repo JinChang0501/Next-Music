68809b4746f60b87ce8b8e5f180961af88ebecca
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-06-25 17:38:49 +0800

    1. 新增-忘記密碼 components 2. 新增-忘記密碼 page

---
+++ pages/login/forget-password.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ForgetPassword from '@/components/login/forget-password'
+
+export default function ForgetPasswordp() {
+  return (
+    <div>
+      <ForgetPassword />
+    </div>
+  )
+}
