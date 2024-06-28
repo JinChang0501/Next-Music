9e68fcd8bf637b15d193b52d0fee6f217f5842a1
Efron <a161026521@gmail.com>
2024-06-28 16:32:01 +0800

    1. complete third Image 2. complete scss accordion

--- pages/ticket/desktop-concert/third.js
+++ pages/ticket/desktop-concert/third.js
@@ -2,6 +2,8 @@ import React from 'react'
 import TicketFormLayout from '@/components/layout/ticket-layout/ticketFormLayout'
 import ProgressBar from '@/components/ticket/progressBar'
 import style from '@/styles/ticket/desktop-concert/third.module.scss'
+import DesktopWhiteNoIconBtnPurple from '@/components/common/button/desktopWhiteButton/desktopWhiteNoIconBtnPurple'
+import Image from 'next/image'
 
 export default function Third() {
   return (
@@ -20,34 +22,52 @@ export default function Third() {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button
-                className="accordion-button"
+                className="accordion-button chb-h5"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#panelsStayOpen-collapseOne"
-                aria-expanded="true"
-                aria-controls="panelsStayOpen-collapseOne"
               >
-                Accordion Item #1
+                請確認票券座位
               </button>
             </h2>
             <div
               id="panelsStayOpen-collapseOne"
               className="accordion-collapse collapse show"
             >
-              <div className="accordion-body">123</div>
+              <div className="accordion-body">
+                {/* image */}
+                <div className={`${style.image}`}>
+                  <Image
+                    src="/images/ticket/desktop-third-size2.jpg"
+                    alt="test"
+                    fill
+                    priority
+                  />
+                </div>
+
+                {/* info */}
+                <div></div>
+
+                {/* ticketArea */}
+                <div></div>
+
+                {/* ticketSeat */}
+                <div></div>
+
+                {/* totalPrice */}
+                <div></div>
+              </div>
             </div>
           </div>
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button
-                className="accordion-button collapsed"
+                className="accordion-button collapsed  chb-h5"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#panelsStayOpen-collapseTwo"
-                aria-expanded="false"
-                aria-controls="panelsStayOpen-collapseTwo"
               >
-                Accordion Item #2
+                請確認購買資訊
               </button>
             </h2>
             <div
@@ -60,14 +80,12 @@ export default function Third() {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button
-                className="accordion-button collapsed"
+                className="accordion-button collapsed  chb-h5"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#panelsStayOpen-collapseThree"
-                aria-expanded="false"
-                aria-controls="panelsStayOpen-collapseThree"
               >
-                Accordion Item #3
+                請確認支付方式
               </button>
             </h2>
             <div
@@ -78,6 +96,7 @@ export default function Third() {
             </div>
           </div>
         </div>
+        <DesktopWhiteNoIconBtnPurple className="w-100" />
       </div>
     </>
   )
