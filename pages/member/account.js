0d2680d6b21834bf921d3201bd8fa54e9aee0c7c
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-06-28 21:11:03 +0800

    更新 - 帳號頁面上的手風琴底色(現在是預設樣式)

--- pages/member/account.js
+++ pages/member/account.js
@@ -120,7 +120,7 @@ export default function Account() {
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingOne">
             <button
-              className="accordion-button bg-purple3"
+              className="accordion-button"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
