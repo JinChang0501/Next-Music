270fda715e6848d7ab15654ed29b6ec732b0c239
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-01 00:11:06 +0800

    畫面改善 - 票頁面上margin 不一樣

--- components/member/desktop-layout/ticket-detail-card.js
+++ components/member/desktop-layout/ticket-detail-card.js
@@ -5,7 +5,7 @@ import styles from './ticket-detail-card.module.scss'
 export default function TicketDetailCard() {
   return (
     <>
-      <div className="row text-center mb-3">
+      <div className="row text-center my-3">
         <div className="col d-flex justify-content-center">
           <img
             src={`/images/member/img/composing-2391033__340.jpg`}
@@ -16,8 +16,10 @@ export default function TicketDetailCard() {
         <div className="col my-auto p-0 chr-h5">A區第5排8號</div>
         <div className="col my-auto p-0 chr-h5">$700</div>
         <div className="col my-auto p-0 mx-auto d-flex justify-content-center">
-          {/* <button className="btn btn-primary">查看票券</button> */}
-          <DesktopWhiteNoIconBtnPurple className="px-4 py-2" text="查看票券" />
+          <DesktopWhiteNoIconBtnPurple
+            className="px-1 px-md-4 py-2"
+            text="查看票券"
+          />
         </div>
       </div>
     </>
