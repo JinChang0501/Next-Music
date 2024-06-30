6db24db23006d719ff6d6acd4c279383959834a7
Castle9707 <paomincc25@gmail.com>
2024-06-30 13:31:23 +0800

    推薦活動卡 修改

--- components/activity/recommond-card.js
+++ components/activity/recommond-card.js
@@ -4,15 +4,17 @@ import PhoneBlackNoIconBtnBlack from '../common/button/phoneBlackButton/phoneBla
 export default function RecommondCard({ imgSrc = "https://i.postimg.cc/zB5Gh92q/temp-Image7-Gw6zu.avif", activity_name = "活動名稱", artist_name = "藝人名稱", }) {
   return (
     <>
-      <div className="col-6 col-md-2">
-        <div className="card bg-dark outline d-flex justify-content-center">
+      <div className="col-6 col-sm-6 col-md-2">
+        <div className="card bg-dark outline img-minW d-flex justify-content-center">
           <img
             src={imgSrc}
-            className="card-img-top"
+            className="card-img-top "
           />
-          <div className="card-body">
-            <div className="card-title chb-h6 text-purple3">{activity_name}</div>
-            <div className="card-text chr-p text-white">{artist_name}</div>
+          <div className="card-body d-flex flex-column justify-content-center p-3">
+            <div className="d-flex flex-column justify-content-start mb-4">
+              <div className="card-title chb-h6 text-purple3">{activity_name}</div>
+              <div className="card-text chr-p text-white">{artist_name}</div>
+            </div>
             <PhoneBlackNoIconBtnBlack text="活動資訊" className='chr-p text-nowrap' />
           </div>
         </div>
@@ -20,6 +22,9 @@ export default function RecommondCard({ imgSrc = "https://i.postimg.cc/zB5Gh92q/
       <style jsx>{`
         .outline {
           border: 1px solid #DBD7FF;
+        }
+        .img-minW {
+          min-width: 160px;
         }
       `}</style>
     </>
