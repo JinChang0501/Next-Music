44829af3b515de807a38da22ce161a9aea517ebe
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-01 00:09:25 +0800

    商品頁面-手機板/電腦版 元件

---
+++ components/member/desktop-layout/order-card.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from 'react'
+import styles from './order-card.module.scss'
+import DesktopWhiteNoIconBtnPurple from '@/components/common/button/desktopWhiteButton/desktopWhiteNoIconBtnPurple'
+import Link from 'next/link'
+
+export default function OrderCard() {
+  return (
+    <>
+      <div className="mb-3">
+        <div className="row mt-3 border border-2 border-purple1">
+          <div className="col-12 bg-purple1 py-2">
+            <div className="px-md-5 d-flex justify-content-between">
+              <p className="text-center p-0 m-0 chb-h6 text-white">
+                訂單編號:0000001
+              </p>
+              <p className="text-center p-0 m-0 chb-h6 text-white">已完成</p>
+            </div>
+          </div>
+
+          <div className="col-12 bg-purple3 py-2 border-top border-2 border-purple2">
+            <div className="row text-center">
+              <div className="col chb-h6">商品圖片</div>
+              <div className="col chb-h6">商品名稱</div>
+              <div className="col chb-h6">件數</div>
+              <div className="col chb-h6">訂單總價</div>
+            </div>
+          </div>
+
+          <div className="col-12 py-2">
+            <div className="row text-center my-1">
+              <div className="col d-flex justify-content-center">
+                <img
+                  src={`/images/member/img/composing-2391033__340.jpg`}
+                  className={styles.img160}
+                  alt=""
+                />
+              </div>
+              <div className="col my-auto p-0 chr-h6">
+                夢幻樂園演唱會報球帽-藍
+              </div>
+              <div className="col my-auto p-0 chr-h6">3件</div>
+              <div className="col my-auto p-0 chr-h6">$ 2100</div>
+            </div>
+          </div>
+          <div className="col-12 bg-purple3 py-2 border-top border-2 border-purple2">
+            <div className="row text-center">
+              <Link href="/member/store-detail">
+                <div className="col chb-h6">檢視其他商品</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
