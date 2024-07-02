2ecd81af17b52bb68187071c9b84cf1a0015bc8b
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-02 15:48:05 +0800

    增加共用元件 - 頁籤 Tab 1. <Tab       tabName="測試"        >>>頁籤上面的文字       tabTarget="test"         >>>這個是拿來綁定用 這裡的test要跟content裡面的div一致       ariaSelected={true}     >>>預設focus (只有第一個頁籤給true)       classNames="col-4"    >>>調整頁籤寬度用 使用col-.. />

---
+++ components/common/tabs/tab.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Tab({ tabName, tabTarget, ariaSelected, classNames }) {
+  return (
+    <>
+      <li className={`nav-item ${classNames}`} role="presentation">
+        <button
+          className={`nav-link w-100 ${ariaSelected ? 'active' : ''}`}
+          id={`${tabTarget}-tab`}
+          data-bs-toggle="tab"
+          data-bs-target={`#${tabTarget}`}
+          type="button"
+          role="tab"
+          aria-controls={tabTarget}
+          aria-selected={ariaSelected}
+        >
+          {tabName}
+        </button>
+      </li>
+    </>
+  )
+}
