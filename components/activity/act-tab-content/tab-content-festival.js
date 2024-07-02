bf58d30f0657e048a44ac7ce8484e3cf3b29b225
Castle9707 <paomincc25@gmail.com>
2024-07-02 23:28:04 +0800

    tab 測試

---
+++ components/activity/act-tab-content/tab-content-festival.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ActivityCard from '@/components/activity/activity-card'
+
+export default function TabContentFestival({ tabTargetFestival }) {
+  return (
+    <>
+      <div
+        className="tab-pane fade"
+        //^^^^^^^^show跟active只需要寫在第一個tab的content裡面
+        id={tabTargetFestival}
+        role="tabpanel"
+        aria-labelledby={`${tabTargetFestival}-tab`}
+      >
+        <ActivityCard />
+        <ActivityCard />
+        <ActivityCard />
+      </div>
+    </>
+  )
+}
--- pages/Activity/index.js
+++ pages/Activity/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Breadcrumbs from '@/components/common/breadcrumb/Breadcrumbs'
 import BannerA from '@/components/activity/banner-a'
 import LeftBar from '@/components/activity/left-bar'
-import IntroTabs from '@/components/activity/intro-tabs'
+import ActivityTabs from '@/components/activity/activity-tabs'
+import Tab from '@/components/common/tabs/tab'
 import ActivityCard from '@/components/activity/activity-card'
+import TabContentAll from '@/components/activity/act-tab-content/tab-content-all'
+import TabContentConcert from '@/components/activity/act-tab-content/tab-content-concert'
+import TabContentFestival from '@/components/activity/act-tab-content/tab-content-festival'
 
 export default function Activity() {
   const breadcrumbsURL = [
@@ -18,12 +22,37 @@ export default function Activity() {
         <div className="row">
           <LeftBar />
           <div className="col-md-9 col-12">
-            {/* <IntroTabs /> */}
+            {/* <ActivityTabs /> */}
+            <ul className="nav nav-tabs mb-3" id="activityTab" role="tablist">
+              <Tab
+                tabName="所有活動"
+                tabTarget="tabTargetAll"
+                ariaSelected={true}
+                classNames="col-6 col-md-3"
+              />
+              <Tab
+                tabName="演唱會"
+                tabTarget="tabTargetConcert"
+                ariaSelected={false}
+                classNames="col-6 col-md-3"
+              />
+              <Tab
+                tabName="音樂祭"
+                tabTarget="tabTargetFestival"
+                ariaSelected={false}
+                classNames="col-6 col-md-3"
+              />
+            </ul>
+            <div className="tab-content" id="myTabContent">
+              <TabContentAll />
+              <TabContentConcert />
+              <TabContentFestival />
+            </div>
+            {/* <ActivityCard />
             <ActivityCard />
             <ActivityCard />
             <ActivityCard />
-            <ActivityCard />
-            <ActivityCard />
+            <ActivityCard /> */}
           </div>
         </div>
       </div>
