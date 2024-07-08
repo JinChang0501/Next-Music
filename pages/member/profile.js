6bb5d3a8b8b0cd74700606ce7166c5a21109fe60
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-08 13:57:05 +0800

    刪除註解

--- pages/member/profile.js
+++ pages/member/profile.js
@@ -310,22 +310,3 @@ Profile.getLayout = function getLayout(page) {
     </MemberDLayout>
   )
 }
-
-// const Account = () => {
-//   const size = useWindowSize()
-//   const isDesktop = size.width >= 768 // 假設 768px 以上為桌面佈局
-
-//   return isDesktop ? (
-//     <MemberDLayout>
-//       <h1>這是桌面佈局</h1>
-//     </MemberDLayout>
-//   ) : (
-//     <MemberPLayout>
-//       <h1>這是移動佈局</h1>
-//     </MemberPLayout>
-//   )
-// }
-
-// Account.getLayout = (page) => page
-
-// export default Account
