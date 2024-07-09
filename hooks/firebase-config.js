8589b40ced78c8186ba3e7cc43f6963bdee36fd0
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-09 19:31:10 +0800

    新增firebase.js makin

---
+++ hooks/firebase-config.js
@@ -0,0 +1,12 @@
+//Firebase 上面貼下來的
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNVAoveL1TRvOwiCMuMFRdz3Y-i7HClSU',
+  authDomain: 'makin-login-53c81.firebaseapp.com',
+  projectId: 'makin-login-53c81',
+  storageBucket: 'makin-login-53c81.appspot.com',
+  messagingSenderId: '265045485903',
+  appId: '1:265045485903:web:8696ae6d7d891f3fe93d3a',
+  measurementId: 'G-ESBNSQY7LJ',
+}
+
+export { firebaseConfig }
