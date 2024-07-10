ad732d7fbca1aea01edbe2b3a9ce9d1e637ac85e
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-10 11:22:55 +0800

    更新文件

--- components/login/backup/login-modal.js
+++ components/login/backup/login-modal.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import { BsGoogle } from 'react-icons/bs'
-import ForgetPasswordModal from './forget-password-modal'
+import ForgetPasswordModal from '../forget-password-modal'
 
 export default function LoginModal() {
   const [isActive, setIsActive] = useState(false)
