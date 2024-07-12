1caf4602a4fbd4841e2630afaf04344c279ff01b
Alice0921Wu <yun9912.aw@gmail.com>
2024-07-12 09:29:33 +0800

    推薦圖片相關

--- components/product/card-product2.js
+++ components/product/card-product2.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import DesktopBlackNoIconBtnPurple from '@/components/common/button/desktopBlackButton/desktopBlackNoIconBtnPurple'
-import data from '@/data/product/Product.json'
+import data from '@/data/product/ProductAd.json'
 import Link from 'next/link'
 // import required modules
 import styles from '@/styles/product/product.module.scss'
@@ -10,77 +10,27 @@ export default function CardProduct2() {
 
   return (
     <>
-      {products.map((v, i) => {
-        return ( 
-       
-            <div key={v.id} className={`row ${styles['row2']}`}>
-              <div className={` col-4 ${styles['mt-52']} `}>
-      
-        <div className={`card ${styles['card']} ${styles['mt-28']}`}>
-          <img
-            src={`/images/product/list/${v.picture}`}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className={`card-body ${styles['bg-bk']}`}>
-            {/* <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}><Link href={`/product/${v.id}`}>{v.name}</Link></p> */}
-            <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}></p>
-            <p className={`card-text chb-h6 text-white ${styles['text-center']}`}></p>
-            <p className={`card-text chb-h6 text-white ${styles['text-center']}`}>NT$ </p>
-            <div className={`${styles['text-center']}`}>
-            <Link href={`/product/`}><DesktopBlackNoIconBtnPurple text="詳細資訊" className={`chb-p`} /></Link>
-            
-            </div>
-          </div>
-        </div>
-   
-              </div>  
-              <div className={` col-4 ${styles['mt-52']}`}>
-      
-        <div className={`card ${styles['card']} ${styles['mt-28']}`}>
-          <img
-            src={`/images/product/list/${v.picture}`}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className={`card-body ${styles['bg-bk']}`}>
-            {/* <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}><Link href={`/product/${v.id}`}>{v.name}</Link></p> */}
-            <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}></p>
-            <p className={`card-text chb-h6 text-white ${styles['text-center']}`}></p>
-            <p className={`card-text chb-h6 text-white ${styles['text-center']}`}>NT$ </p>
-            <div className={`${styles['text-center']}`}>
-            <Link href={`/product/`}><DesktopBlackNoIconBtnPurple text="詳細資訊" className={`chb-p`} /></Link>
-            
-            </div>
-          </div>
-        </div>
-   
-              </div>  
-              <div className={` col-4 ${styles['mt-52']}`}>
-      
-        <div className={`card ${styles['card']} ${styles['mt-28']}`}>
-          <img
-            src={`/images/product/list/`}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className={`card-body ${styles['bg-bk']}`}>
-            {/* <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}><Link href={`/product/${v.id}`}>{v.name}</Link></p> */}
-            <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}></p>
-            <p className={`card-text chb-h6 text-white ${styles['text-center']}`}></p>
-            <p className={`card-text chb-h6 text-white ${styles['text-center']}`}>NT$ </p>
-            <div className={`${styles['text-center']}`}>
-            <Link href={`/product/`}><DesktopBlackNoIconBtnPurple text="詳細資訊" className={`chb-p`} /></Link>
-            
-            </div>
-          </div>
-        </div>
-   
-              </div>  
-              
-            </div>
-          
-        ) })}
+       {products.map((v, i) => {
+              return (
+                <div key={v.id} className={`card ${styles['card']} ${styles['mt-28']}`}>
+                  <img
+                    src={`/images/product/list/${v.picture}`}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className={`card-body ${styles['bg-bk']}`}>
+                    {/* <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}><Link href={`/product/${v.id}`}>{v.name}</Link></p> */}
+                    <p className={`card-text chb-h6 text-purple3 ${styles['text-center']}`}>{v.name}</p>
+                    <p className={`card-text chb-h6 text-white ${styles['text-center']}`}>{v.activity}</p>
+                    <p className={`card-text chb-h6 text-white ${styles['text-center']}`}>NT$ {v.price}</p>
+                    <div className={`${styles['text-center']}`}>
+                    <Link href={`/product/${v.id}`}><DesktopBlackNoIconBtnPurple text="詳細資訊" className={`chb-p`} /></Link>
+                    
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
     </>
   )
 }
