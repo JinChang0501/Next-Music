d98a74aaa3df0867a43cdb1c14bf99703d0a1636
Castle9707 <paomincc25@gmail.com>
2024-07-13 14:21:10 +0800

    活動卡手機版字體調整

--- components/activity/activity-card.js
+++ components/activity/activity-card.js
@@ -66,7 +66,7 @@ export default function ActivityCard({ imgSrc = "https://i.postimg.cc/zB5Gh92q/t
                 </div>
                 <div className="d-flex my-2">
                   <BsCalendar4 className="p-0 me-2 me-md-3" />
-                  <div className="card-text chb-p p-0">{actdate}&nbsp;{acttime}</div>
+                  <div className="card-text chb-p p-0">{actdate}</div>
                 </div>
               </div>
               <div className="col-4 d-flex justify-content-end gap-2 text-nowrap">
@@ -100,6 +100,16 @@ export default function ActivityCard({ imgSrc = "https://i.postimg.cc/zB5Gh92q/t
       <style jsx>{`
         .outline {
           border: 1px solid #DBD7FF;
+        }
+        @media (max-width: 390px){
+          .chb-h4 {
+            font-size: 18px;
+            letter-spacing: 1.8px;
+          }
+          .chb-p {
+            font-size: 12px;
+            font-weight: 400;
+          }
         }
       `}</style>
     </>
