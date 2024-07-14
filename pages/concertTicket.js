b05050c4ce42dd4b202e30041dc67b048920afd1
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-14 16:21:28 +0800

    concertTicketNew 測試Page頁

---
+++ pages/concertTicket.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ConcertTicketNew from '@/components/ticket/desktop-concert/fourth/concertTicket/concertTicket'
+
+export default function ConcertTicketPage() {
+  return (
+    <>
+      <ConcertTicketNew />
+    </>
+  )
+}
