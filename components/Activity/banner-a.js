ab131ab60ca87af3175b019d55a432f0928ee76c
Castle9707 <paomincc25@gmail.com>
2024-07-15 23:24:13 +0800

    prettier

--- components/Activity/banner-a.js
+++ components/Activity/banner-a.js
@@ -8,78 +8,78 @@ export default function BannerA() {
     <>
       {/* banner carousel start */}
       <div
-        id="myCarousel"
-        className="carousel slide mb-0"
-        data-bs-ride="carousel"
+        id='myCarousel'
+        className='carousel slide mb-0'
+        data-bs-ride='carousel'
       >
         {/* banner switch 群組 */}
-        <div className="carousel-indicators">
+        <div className='carousel-indicators'>
           {/* carousel-indicator 樣式待修改 方塊 */}
           <button
-            type="button"
-            data-bs-target="#myCarousel"
+            type='button'
+            data-bs-target='#myCarousel'
             data-bs-slide-to={0}
-            className="cube"
-            aria-label="Slide 1"
+            className='cube'
+            aria-label='Slide 1'
           />
           <button
-            type="button"
-            data-bs-target="#myCarousel"
+            type='button'
+            data-bs-target='#myCarousel'
             data-bs-slide-to={1}
-            aria-label="Slide 2"
-            className="cube"
+            aria-label='Slide 2'
+            className='cube'
           />
           <button
-            type="button"
-            data-bs-target="#myCarousel"
+            type='button'
+            data-bs-target='#myCarousel'
             data-bs-slide-to={2}
-            aria-label="Slide 3"
-            className="cube active"
-            aria-current="true"
+            aria-label='Slide 3'
+            className='cube active'
+            aria-current='true'
           />
         </div>
         {/* banner 圖 */}
-        <div className="carousel-inner">
-          <div className="carousel-item active">
+        <div className='carousel-inner'>
+          <div className='carousel-item active'>
             <img
               src={bn1}
-              className="d-block w-100"
-              alt=""
+              className='d-block w-100'
+              alt=''
             />
           </div>
-          <div className="carousel-item">
+          <div className='carousel-item'>
             <img
               src={bn2}
-              className="d-block w-100"
-              alt=""
+              className='d-block w-100'
+              alt=''
             />
           </div>
-          <div className="carousel-item">
+          <div className='carousel-item'>
             <img
               src={bn3}
-              className="d-block w-100"
-              alt=""
+              className='d-block w-100'
+              alt=''
             />
           </div>
         </div>
         {/* 左右控制鈕 */}
         <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide="prev"
+          className='carousel-control-prev'
+          type='button'
+          data-bs-target='#myCarousel'
+          data-bs-slide='prev'
         >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="visually-hidden">Previous</span>
+          <span className='carousel-control-prev-icon' aria-hidden='true' />
+          <span className='visually-hidden'>Previous</span>
         </button>
         <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide="next"
+          className='carousel-control-next'
+          type='button'
+          data-bs-target='#myCarousel'
+          data-bs-slide='next'
         >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="visually-hidden">Next</span>
+          <span className='carousel-control-next-icon' aria-hidden='true' />
+          <span className='visually-hidden'>Next</span>
         </button>
       </div>
       {/* banner carousel end */}
--- components/activity/activity-tabs-n.js
+++ components/activity/activity-tabs-n.js
@@ -6,46 +6,46 @@ export default function ActivityTabs() {
   const { handleTabChange, activeTab } = useTabs()
   return (
     <>
-      <ul className="nav nav-tabs mb-3" id="activityTab" role="tablist">
-        <li className="nav-item col-4 col-md-3" role="presentation">
+      <ul className='nav nav-tabs mb-3' id='activityTab' role='tablist'>
+        <li className='nav-item col-4 col-md-3' role='presentation'>
           <button
             className={`nav-link w-100 ${activeTab === 'activity' ? 'active' : ''
               } px-5`}
-            id="activity-tab"
-            data-bs-toggle="tab"
-            type="button"
-            role="tab"
-            aria-controls="activity"
+            id='activity-tab'
+            data-bs-toggle='tab'
+            type='button'
+            role='tab'
+            aria-controls='activity'
             aria-selected={activeTab === 'activity'}
             onClick={() => handleTabChange('activity')}
           >
             所有活動
           </button>
         </li>
-        <li className="nav-item col-4 col-md-3" role="presentation">
+        <li className='nav-item col-4 col-md-3' role='presentation'>
           <button
             className={`nav-link w-100 ${activeTab === 'concert' ? 'active' : ''
               } px-5`}
-            id="concert-tab"
-            data-bs-toggle="tab"
-            type="button"
-            role="tab"
-            aria-controls="concert"
+            id='concert-tab'
+            data-bs-toggle='tab'
+            type='button'
+            role='tab'
+            aria-controls='concert'
             aria-selected={activeTab === 'concert'}
             onClick={() => handleTabChange('concert')}
           >
             演唱會
           </button>
         </li>
-        <li className="nav-item col-4 col-md-3" role="presentation">
+        <li className='nav-item col-4 col-md-3' role='presentation'>
           <button
             className={`nav-link w-100 ${activeTab === 'festival' ? 'active' : ''
               } px-5`}
-            id="festival-tab"
-            data-bs-toggle="tab"
-            type="button"
-            role="tab"
-            aria-controls="festival"
+            id='festival-tab'
+            data-bs-toggle='tab'
+            type='button'
+            role='tab'
+            aria-controls='festival'
             aria-selected={activeTab === 'festival'}
             onClick={() => handleTabChange('festival')}
           >
