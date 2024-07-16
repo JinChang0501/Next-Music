1adaca46561e24c06bbdc611b841f078da244d31
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-16 13:10:11 +0800

    store-detail MAP=> DONE 1.增加判斷是否有配送門市，顯示711-storename : 顯示宅配

--- components/member/desktop-layout/store-detail.js
+++ components/member/desktop-layout/store-detail.js
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { BsArrowLeftCircle } from 'react-icons/bs'
 import StoreDetailCard from './store-detail-card'
 import Link from 'next/link'
-export default function StoreDetail() {
+import moment from 'moment-timezone'
+
+export default function StoreDetail({ storeData }) {
   const [isMobile, setIsMobile] = useState(false)
+
+  console.log('下面是storeData--------------')
+  console.log(storeData)
 
   useEffect(() => {
     const handleResize = () => {
@@ -16,6 +21,14 @@ export default function StoreDetail() {
 
     return () => window.removeEventListener('resize', handleResize) // 清除事件監聽器
   }, [])
+
+  if (!storeData || storeData.length === 0) {
+    return <div>Loading...</div>
+  }
+
+  const formateCreated_At = moment(storeData[0].created_at)
+    .tz('Asia/Taipei')
+    .format('YYYY/MM/DD HH:mm')
 
   return (
     <>
@@ -37,7 +50,7 @@ export default function StoreDetail() {
         <div className="col-12 bg-purple1 py-2">
           <div className="px-md-5 m-0 fs-3 d-flex justify-content-between">
             <p className="text-center p-0 m-0 chb-h5 text-white">
-              訂單編號:0000001
+              訂單編號:{storeData[0].order_num}
             </p>
             <p className="text-center p-0 m-0 chb-h5 text-white">已完成</p>
           </div>
@@ -54,13 +67,27 @@ export default function StoreDetail() {
 
         {/* MAP寫在這 */}
         <div className="col-12 py-2">
-          <StoreDetailCard />
+          {storeData.map((v, i) => {
+            return (
+              <StoreDetailCard
+                key={i}
+                picture={v.picture}
+                price={v.price}
+                productName={v.productName}
+                quantity={v.quantity}
+              />
+            )
+          })}
         </div>
 
         <div className="col-12 py-2 border-top border-2 border-purple2">
           <div className="px-sm-5 m-0 d-flex justify-content-between">
-            <p className="text-center p-0 m-0 chb-h5">共 3 樣商品</p>
-            <p className="text-center p-0 m-0 chb-h5">總金額: $2100</p>
+            <p className="text-center p-0 m-0 chb-h5">
+              共 {storeData[0].productTotalCount} 樣商品
+            </p>
+            <p className="text-center p-0 m-0 chb-h5">
+              總金額: ${storeData[0].totalPrice}
+            </p>
           </div>
         </div>
 
@@ -73,19 +100,27 @@ export default function StoreDetail() {
         <div className="col-12 py-2">
           <div className="px-sm-5 m-0 d-flex justify-content-between mb-2">
             <p className="text-center p-0 m-0 chb-h5">訂單時間</p>
-            <p className="text-center p-0 m-0 chr-h5">2024/06/31 19:30</p>
+            <p className="text-center p-0 m-0 chr-h5">{formateCreated_At}</p>
           </div>
           <div className="px-sm-5 m-0 d-flex justify-content-between mb-2">
             <p className="text-center p-0 m-0 chb-h5">訂購人</p>
-            <p className="text-center p-0 m-0 chr-h5">黃大安</p>
+            <p className="text-center p-0 m-0 chr-h5">
+              {storeData[0].memberName}
+            </p>
           </div>
           <div className="px-sm-5 m-0 d-flex justify-content-between mb-2">
             <p className="text-center p-0 m-0 chb-h5">付款方式</p>
-            <p className="text-center p-0 m-0 chr-h5">LINE PAY</p>
+            <p className="text-center p-0 m-0 chr-h5">
+              {storeData[0].payment_method}
+            </p>
           </div>
           <div className="px-sm-5 m-0 d-flex justify-content-between mb-2">
             <p className="text-center p-0 m-0 chb-h5">配送方式</p>
-            <p className="text-center p-0 m-0 chr-h5">7-11 大安店</p>
+            <p className="text-center p-0 m-0 chr-h5">
+              {storeData[0].storename
+                ? `${storeData[0].pickup_method}-${storeData[0].storename}`
+                : `${storeData[0].pickup_method}`}
+            </p>
           </div>
         </div>
       </div>
--- pages/member/store-detail/[order_num].js
+++ pages/member/store-detail/[order_num].js
@@ -2,9 +2,52 @@ import MemberDLayout from '@/components/member/desktop-layout'
 import React, { useEffect, useState } from 'react'
 import StoreDetail from '@/components/member/desktop-layout/store-detail'
 import StoreDetailMobile from '@/components/member/mobile-layout/store-detail-mobile'
+import { GET_STORE_DETAIL } from '@/configs/api-path'
+import { useRouter } from 'next/router'
 
 export default function StoreDetailPage() {
   const [isDesktop, setIsDesktop] = useState(true)
+  const [storeData, setStoreData] = useState([])
+  const router = useRouter()
+  const [data, setData] = useState({
+    success: false,
+    data: {},
+  })
+  useEffect(() => {
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    const order_num = router.query.order_num
+
+    console.log('下面是router.query')
+
+    console.log(router.query)
+
+    const url = `${GET_STORE_DETAIL}/${order_num}`
+    // new URLSearchParams(router.query) 這塊應該是搜尋結果的query string
+    fetch(url, {
+      signal,
+      credentials: 'include',
+    })
+      .then((r) => r.json())
+      .then((myData) => {
+        console.log('--------------------------')
+        console.log(data)
+        setData(myData)
+        console.log('----------myData---------')
+        console.log(myData)
+        console.log('----------myData.data.result---------')
+
+        setStoreData(myData.data.result) //物件陣列
+        console.log(myData.data.result)
+      })
+      .catch((ex) => {
+        console.log('fetch-ex', ex)
+      })
+    return () => {
+      controller.abort() // 取消上一次的 ajax
+    }
+  }, [router])
 
   useEffect(() => {
     const handleResize = () => {
@@ -18,7 +61,15 @@ export default function StoreDetailPage() {
     return () => window.removeEventListener('resize', handleResize) // 清除事件監聽器
   }, [])
 
-  return <>{isDesktop ? <StoreDetail /> : <StoreDetailMobile />}</>
+  return (
+    <>
+      {isDesktop ? (
+        <StoreDetail storeData={storeData} />
+      ) : (
+        <StoreDetailMobile storeData={storeData} />
+      )}
+    </>
+  )
 }
 StoreDetailPage.getLayout = function getLayout(page) {
   return (
