a28b0a0d62dacfd34235fc13df36b71a4cd697ff
Castle9707 <paomincc25@gmail.com>
2024-07-22 11:48:01 +0800

    top tracks 切版

---
+++ components/artist/top-track-list.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopTrackItem from './top-track-item'
+
+export default function TopTrackList() {
+  return (
+    <>
+      <TopTrackItem />
+      <TopTrackItem />
+      <TopTrackItem />
+      <TopTrackItem />
+      <TopTrackItem />
+    </>
+  )
+}
