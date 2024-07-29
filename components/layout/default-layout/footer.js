1de0e9482a287fe81e37a1a4e8ebf4d9e87385f5
Castle9707 <paomincc25@gmail.com>
2024-07-29 10:13:58 +0800

    default layout footer 改色

--- components/layout/default-layout/footer.js
+++ components/layout/default-layout/footer.js
@@ -4,8 +4,8 @@ import { FaMusic } from 'react-icons/fa6'
 export default function Footer() {
   return (
     <>
-      <footer className="footer bg-purple2 py-3 text-center mt-auto">
-        <span className="text-body-secondary fw-bolder">
+      <footer className="footer bg-black80 py-3 text-center mt-auto">
+        <span className="text-white fw-bolder">
           <FaMusic />
           &nbsp;&nbsp;&nbsp; Enjoy yourself in music&nbsp;&nbsp;&nbsp;
           <FaMusic />
