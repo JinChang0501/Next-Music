a95e7ebaecaf1f3bb4e46da6dc97edeaf9355e33
Castle9707 <paomincc25@gmail.com>
2024-07-29 09:45:09 +0800

    活動列表頁BN修改

--- components/Activity/banner-a.js
+++ components/Activity/banner-a.js
@@ -1,85 +1,74 @@
 import React from 'react'
 
-export default function BannerA() {
-  const bn1 = 'https://i.postimg.cc/tJRCk5qv/Banner01.jpg'
-  const bn2 = 'https://i.postimg.cc/Nfzf8mWb/Banner03.jpg'
-  const bn3 = 'https://i.postimg.cc/8ky1DLPD/Banner12.jpg'
+export default function BannerA({
+  bn1 = '/images/Activity/banner/Banner13.jpg',
+  bn2 = '/images/Activity/banner/Banner10.jpg',
+  bn3 = '/images/Activity/banner/Banner06.jpg',
+}) {
   return (
     <>
       {/* banner carousel start */}
       <div
-        id='myCarousel'
-        className='carousel slide mb-0'
-        data-bs-ride='carousel'
+        id="myCarousel"
+        className="carousel slide mb-0"
+        data-bs-ride="carousel"
       >
         {/* banner switch 群組 */}
-        <div className='carousel-indicators'>
+        <div className="carousel-indicators">
           {/* carousel-indicator 樣式待修改 方塊 */}
           <button
-            type='button'
-            data-bs-target='#myCarousel'
+            type="button"
+            data-bs-target="#myCarousel"
             data-bs-slide-to={0}
-            className='cube'
-            aria-label='Slide 1'
+            className="cube"
+            aria-label="Slide 1"
           />
           <button
-            type='button'
-            data-bs-target='#myCarousel'
+            type="button"
+            data-bs-target="#myCarousel"
             data-bs-slide-to={1}
-            aria-label='Slide 2'
-            className='cube'
+            aria-label="Slide 2"
+            className="cube"
           />
           <button
-            type='button'
-            data-bs-target='#myCarousel'
+            type="button"
+            data-bs-target="#myCarousel"
             data-bs-slide-to={2}
-            aria-label='Slide 3'
-            className='cube active'
-            aria-current='true'
+            aria-label="Slide 3"
+            className="cube active"
+            aria-current="true"
           />
         </div>
         {/* banner 圖 */}
-        <div className='carousel-inner'>
-          <div className='carousel-item active'>
-            <img
-              src={bn1}
-              className='d-block w-100'
-              alt=''
-            />
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={bn1} className="d-block w-100" alt="" />
           </div>
-          <div className='carousel-item'>
-            <img
-              src={bn2}
-              className='d-block w-100'
-              alt=''
-            />
+          <div className="carousel-item">
+            <img src={bn2} className="d-block w-100" alt="" />
           </div>
-          <div className='carousel-item'>
-            <img
-              src={bn3}
-              className='d-block w-100'
-              alt=''
-            />
+          <div className="carousel-item">
+            <img src={bn3} className="d-block w-100" alt="" />
           </div>
         </div>
         {/* 左右控制鈕 */}
         <button
-          className='carousel-control-prev'
-          type='button'
-          data-bs-target='#myCarousel'
-          data-bs-slide='prev'
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide="prev"
         >
-          <span className='carousel-control-prev-icon' aria-hidden='true' />
-          <span className='visually-hidden'>Previous</span>
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          className='carousel-control-next'
-          type='button'
-          data-bs-target='#myCarousel'
-          data-bs-slide='next'
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide="next"
         >
-          <span className='carousel-control-next-icon' aria-hidden='true' />
-          <span className='visually-hidden'>Next</span>
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
       {/* banner carousel end */}
