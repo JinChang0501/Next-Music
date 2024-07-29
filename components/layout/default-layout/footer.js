3bafcbaa3519217ce95ad6f327cdef825b60e67f
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-29 10:00:18 +0800

    Footer => MemberDLayout改淺灰

--- components/layout/default-layout/footer.js
+++ components/layout/default-layout/footer.js
@@ -4,7 +4,7 @@ import { FaMusic } from 'react-icons/fa6'
 export default function Footer() {
   return (
     <>
-      <footer className="footer bg-purple2 py-3 text-center mt-auto">
+      <footer className="footer bg-light py-3 text-center mt-auto">
         <span className="text-body-secondary fw-bolder">
           <FaMusic />
           &nbsp;&nbsp;&nbsp; Enjoy yourself in music&nbsp;&nbsp;&nbsp;
